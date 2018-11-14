e57edb8579e90e4d4a9ac2f877e8e5cb63bcca65
Alexey <nuzhaleks2@rambler.ru>
2018-11-14 20:52:20 +0300

    Chage map legend on main page to interactive buttons bar filter. Solve issue #270

--- src/components/maps/Mainpage.vue
+++ src/components/maps/Mainpage.vue
@@ -13,12 +13,17 @@
           coordinates: [50.351, 142.395],
           id: 'map-mainpage',
           object: null,
+          eventSteps: [24, 120, 336],
           events: []
         }
       }
     },
     methods: {
       addEvents: function(events) {
+        const markersBasePane = this.map.object.getPane('markerPane')
+        for (let i = 0; i <= this.map.eventSteps.length; i++) {
+          this.map.object.createPane('markers_' + i, markersBasePane).style.zIndex = 600 + (i - 1) % (this.map.eventSteps.length + 1)
+        }
         events.reverse().forEach(event => {
           const datetime = this.$moment(event.locValues.data.event_datetime)
           const datetimeDiff = this.$moment.utc().diff(datetime, 'hours')
@@ -28,7 +33,10 @@
           const longitude = event.locValues.data.lon
           const magnitude = event.locValues.data.mag.toFixed(1)
           const magnitudeType = event.locValues.data.mag_t
+          const etn = this.eventTimeGroup(datetimeDiff) + 1
+          const paneName = 'markers_' + etn
           const options = {
+            pane: paneName,
             color: 'black',
             colorOpacity: 1.0,
             fillColor: this.eventColor(datetimeDiff),
@@ -79,13 +87,29 @@
 
         legend.onAdd = function() {
           let div = window.L.DomUtil.create('div', 'map-legend map-legend-mainpage')
-          div.innerHTML += '<span style="background:#FF0000;">< 24 ч</span>'
-          div.innerHTML += '<span style="background:#FFA500">1-5 дн</span>'
-          div.innerHTML += '<span style="background:#FFFF00">6-14 дн</span>'
-          div.innerHTML += '<span style="background:#808080">> 14 дн</span>'
+          const description = [
+            { text: '< 24 ч', class: 'less-1-day' },
+            { text: '1-5 дн', class: 'less-5-days' },
+            { text: '6-14 дн', class: 'less-14-days' },
+            { text: '> 14 дн', class: 'more-14-days' }
+          ]
+          for (let i = 0; i <= this.map.eventSteps.length; i++) {
+            let item = window.L.DomUtil.create('span', 'button pressed ' + description[i].class, div)
+            item.role = 'button'
+            item.textContent = description[i].text
+            item.dataset.group = (i + 1) % description.length
+            item.onclick = e => {
+              e.preventDefault()
+              let g = +e.currentTarget.dataset.group
+              let p = this.map.object.getPane('markers_' + g)
+              p.classList.toggle('invisible')
+              e.currentTarget.classList.toggle('pressed')
+              return false
+            }
+          }
 
           return div
-        }
+        }.bind(this)
 
         legend.addTo(this.map.object)
 
@@ -102,16 +126,16 @@
       createMap: function() {
         this.map.object = createMap(this.map.id, this.map.coordinates, 5, false)
       },
-      eventColor: function(timeDifference) {
-        if (timeDifference <= 24) {
-          return '#ff0000'
-        } else if (timeDifference > 24 && timeDifference <= 120) {
-          return '#ffa500'
-        } else if (timeDifference > 120 && timeDifference <= 336) {
-          return '#ffff00'
-        } else {
-          return '#808080'
+      eventTimeGroup: function(timeDifference) {
+        for (let i = 0; i < this.map.eventSteps.length; i++) {
+          if (timeDifference <= this.map.eventSteps[i]) {
+            return i
+          }
         }
+        return -1
+      },
+      eventColor: function(timeDifference) {
+        return ['#808080', '#ff0000', '#ffa500', '#ffff00'][this.eventTimeGroup(timeDifference) + 1]
       },
       eventRadius: function(magnitude) {
         if (magnitude < 3.0) {
